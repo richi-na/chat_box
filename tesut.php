bdeb34bbd78eafde860c1e181db3660693782fe0
richi-na <69388884+richi-na@users.noreply.github.com>
2020-10-31 00:28:47 +0900

    Update tesut.php

--- tesut.php
+++ tesut.php
@@ -1,8 +1,8 @@
 <?php
 //データベースに接続
- $dsn = 'mysql:dbname=tb220242db;host=localhost'; 
- $user = 'tb-220242'; 
- $password = 'N95N7ba6UV'; 
+ $dsn = 'xxxxxxxxx'; 
+ $user = 'xxxxxxxxx'; 
+ $password = 'xxxxxxxxx'; 
  $dbh = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING)); 
 
  //テーブル作成
